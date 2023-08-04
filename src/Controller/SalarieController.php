1b271ef88dd50c8260b51aaa8a07533ba2c5d47c
archers <badarcher.so@gmail.com>
2023-08-04 14:08:12 +0200

    mise en ligne avec tableau salarie

--- src/Controller/SalarieController.php
+++ src/Controller/SalarieController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Entity\Employe;
 use App\Form\FormsalarieType;
+use App\Repository\EmployeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,30 +22,37 @@ class SalarieController extends AbstractController
         ]);
     }
 
-    #[Route('/salarie/ajout', name: 'salarie_ajout')]
-    // Request permet de recupeer les GLOBAL, 
+    #[Route('/salarie/salarie', name: 'salarie_form')]
+    // Request permet de recuperer les GLOBAL, 
     public function ajout(Request $request, EntityManagerInterface $manager): Response
     {
         $employe = new Employe;
-    // je crée une variable dans laquelle je stocke mon formulaire crée grace à createForm() et a son formBuilder (FormsalarieType)
-    // click droit pour importer la class
+        // je crée une variable dans laquelle je stocke mon formulaire crée grace à createForm() et a son formBuilder (FormsalarieType)
+        // click droit pour importer la class
 
-    // 
+        // 
         $form = $this->createForm(FormsalarieType::class, $employe);
 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $manager->persist($employe);
             // persist() permet de préparer les requette SQL par rapport à l'objet qu'on lui donne enn paramètre
             $manager->flush();
             // flus() permet d'executer toutes les requettes précédentes
-            return $this->redirectToRoute('salarie_ajout');
-
+            return $this->redirectToRoute('app_salarie');
         }
         // dans render le se second le premier parametre est le chemin le SECOND EST UN TABLEAU
-        return $this->render('/salarie/salarie.html.twig', [
+        return $this->render('salarie/salarie.html.twig', [
             'formsalarie' => $form,
+        ]);
+    }
+    #[Route('/salarie/show', name: 'showsalarie')]
+    public function showsalarie( EmployeRepository $repo): Response
+    {
+        $employe = $repo->findAll();
+        return $this->render('salarie/show.html.twig', [
+            'formsalarie' => $employe,
+            // 'SalarieController' => 'Bienvenue au salarié ',
         ]);
     }
 }
